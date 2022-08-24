730e6a1feb37ee0f2d06cfd452ff0d004ed8ba9b
misskriiss <krizhukfu@gmail.com>
2022-08-24 21:28:56 +0300

    added list

---
+++ src/test/java/list.java
@@ -0,0 +1,11 @@
+public enum list {
+    FIRST("Соковыжималка шнековая Kitfort КТ-1102-1"),
+    SECOND("Соковыжималка шнековая Kitfort КТ-1106-1 красный"),
+    THIRD("Соковыжималка шнековая Kitfort КТ-1105-1 красный");
+    public final String desc;
+
+    list(String desc) {
+        this.desc = desc;
+    }
+}
+
